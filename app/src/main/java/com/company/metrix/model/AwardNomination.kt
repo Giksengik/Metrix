da320027b2afac27b117124d3edcf2c60930a5c7
Giksengik <giksengik@gmail.com>
2021-09-25 18:48:14 +0300

    Awards ui

---
+++ app/src/main/java/com/company/metrix/model/AwardNomination.kt
@@ -0,0 +1,6 @@
+package com.company.metrix.model
+
+data class AwardNomination (
+    val type : AwardType,
+    val isReceived : Boolean
+    )
---
+++ app/src/main/java/com/company/metrix/services/awards/AwardsListAdapter.kt
@@ -0,0 +1,50 @@
+package com.company.metrix.services.awards
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.company.metrix.databinding.AwardsItemBinding
+import com.company.metrix.model.AwardInfo
+
+class AwardsListAdapter :
+    ListAdapter<AwardInfo, AwardsListAdapter.ViewHolder>(AwardInfoDiffUtil()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder =
+        ViewHolder(
+            AwardsItemBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    class AwardInfoDiffUtil : DiffUtil.ItemCallback<AwardInfo>() {
+        override fun areItemsTheSame(oldItem: AwardInfo, newItem: AwardInfo): Boolean =
+            oldItem.name == newItem.name
+
+        override fun areContentsTheSame(oldItem: AwardInfo, newItem: AwardInfo): Boolean =
+            oldItem == newItem
+
+    }
+
+    class ViewHolder(val binding: AwardsItemBinding) : RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(awardInfo: AwardInfo) {
+            binding.awardName.text = awardInfo.name
+            val awardNominationsAdapter = AwardNominationListAdapter()
+            binding.awardNominationsList.apply{
+                adapter = awardNominationsAdapter
+                layoutManager = LinearLayoutManager(binding.root.context,
+                LinearLayoutManager.HORIZONTAL, false)
+            }
+            awardNominationsAdapter.submitList(awardInfo.nominations)
+        }
+    }
+}
