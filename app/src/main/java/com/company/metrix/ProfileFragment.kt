003a8e6795bdb17ef8f40bdab28da519560cf171
fedor-jedi <fedor.contact@yandex.ru>
2021-09-25 19:08:19 +0300

    Demo profile creation

--- app/src/main/java/com/company/metrix/ProfileFragment.kt
+++ app/src/main/java/com/company/metrix/ProfileFragment.kt
@@ -6,10 +6,16 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.company.metrix.databinding.FragmentProfileBinding
+import com.company.metrix.model.Employee
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class ProfileFragment : Fragment() {
 
     private var binding : FragmentProfileBinding? = null
+    private lateinit var database: DatabaseReference
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -17,6 +23,28 @@ class ProfileFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentProfileBinding.inflate(inflater)
+        database = Firebase.database.reference
+
+        loadEmployeeToDatabase()
+
         return binding?.root
+    }
+
+    private fun loadEmployeeToDatabase()  {
+        val em = Employee(
+            id = 0,
+            team = 0,
+            ratings = listOf(3.0, 2.0, 5.0),
+            strongSkills = listOf("communication", "works_hard"),
+            weakSkills = listOf("code_skill", "speed"),
+            achievements = listOf("friendly")
+        )
+        val user = database.child(Firebase.auth.currentUser!!.uid)
+        user.child("id").setValue(em.id)
+        user.child("team").setValue(em.team)
+        user.child("ratings").setValue(em.ratings)
+        user.child("strongSkills").setValue(em.strongSkills)
+        user.child("weakSkills").setValue(em.weakSkills)
+        user.child("achievements").setValue(em.achievements)
     }
 }
