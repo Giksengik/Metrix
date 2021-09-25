16d7617a4c9e0efe70d8669f01725b3c41822daf
Giksengik <giksengik@gmail.com>
2021-09-25 09:26:04 +0300

    ui fix

--- app/src/main/java/com/company/metrix/MainFragment.kt
+++ app/src/main/java/com/company/metrix/MainFragment.kt
@@ -2,13 +2,18 @@ package com.company.metrix
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.res.ResourcesCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.company.metrix.databinding.FragmentMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationItemView
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 
 class MainFragment : Fragment() {
     private var binding : FragmentMainBinding? = null
@@ -25,6 +30,7 @@ class MainFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         setupNav()
     }
+
 
     private fun setupNav(){
         val fragment = childFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
