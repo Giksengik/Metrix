c68cb8f344b8e2c5fd011febb10b831c0b6d019d
Giksengik <giksengik@gmail.com>
2021-09-25 10:15:06 +0300

    update ui

--- app/src/main/java/com/company/metrix/ServicesFragment.kt
+++ app/src/main/java/com/company/metrix/ServicesFragment.kt
@@ -5,18 +5,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.company.metrix.databinding.FragmentFavouriteBinding
+import com.company.metrix.databinding.FragmentServicesBinding
 
-class FavouriteFragment : Fragment() {
+class ServicesFragment : Fragment() {
 
-    private var binding : FragmentFavouriteBinding? = null
+    private var binding : FragmentServicesBinding? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentFavouriteBinding.inflate(inflater)
+        binding = FragmentServicesBinding.inflate(inflater)
         return binding?.root
     }
 
