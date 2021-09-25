18fcd3ae2120bd3364575334ee5631c570742554
fedor-jedi <fedor.contact@yandex.ru>
2021-09-25 19:08:19 +0300

    Demo profile creation

--- app/src/main/java/com/company/metrix/model/Employee.kt
+++ app/src/main/java/com/company/metrix/model/Employee.kt
@@ -3,7 +3,7 @@ package com.company.metrix.model
 data class Employee(
     val id: Int,
     val team: Int,
-    val ratings: List<Float>,
+    val ratings: List<Double>,
     val strongSkills: List<String>,
     val weakSkills : List<String>,
     val achievements: List<String>
