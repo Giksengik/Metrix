194c67a037cbd978923f5b6a820b3c7cdfe15423
Giksengik <giksengik@gmail.com>
2021-09-25 15:45:07 +0300

    make ui for diagnostic

--- app/src/main/java/com/company/metrix/services/diagnostic/FragmentDiagnostic.kt
+++ app/src/main/java/com/company/metrix/services/diagnostic/FragmentDiagnostic.kt
@@ -5,11 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.company.metrix.databinding.FragmentAwardsBinding
 import com.company.metrix.databinding.FragmentDiagnosticBinding
+import com.company.metrix.model.Question
 
 class FragmentDiagnostic : Fragment() {
+
     private var binding : FragmentDiagnosticBinding? = null
+    private var questionAdapter : DiagnosticQuestionListAdapter? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -19,4 +23,34 @@ class FragmentDiagnostic : Fragment() {
         binding = FragmentDiagnosticBinding.inflate(inflater)
         return binding?.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        setupBackButton()
+        setupQuestionsAdapter()
+    }
+
+    private fun setupBackButton() {
+        binding?.backButton?.setOnClickListener{
+            activity?.onBackPressed()
+        }
+    }
+
+    private fun setupQuestionsAdapter(){
+        questionAdapter = DiagnosticQuestionListAdapter()
+        binding?.diagnosticList?.apply{
+            layoutManager = LinearLayoutManager(activity)
+            adapter = questionAdapter
+        }
+        addDummyQuestions()
+    }
+
+    private fun addDummyQuestions(){
+        questionAdapter?.submitList(
+            listOf("ABOBOBOBO", "KGSJGIOSDPOGJSOG", "OISJDGOPGJSOPDG",
+            "OISIGJDGOJPSODG", "MSGIDJGSD").map{
+                Question(it)
+            }
+        )
+    }
+
 }
