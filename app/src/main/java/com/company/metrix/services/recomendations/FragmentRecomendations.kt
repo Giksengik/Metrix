2da4573038a7352958f042473b8ce0fc9bb1583e
fedor-jedi <fedor.contact@yandex.ru>
2021-09-26 03:53:41 +0300

    Implemented recommendations view

--- app/src/main/java/com/company/metrix/services/recomendations/FragmentRecomendations.kt
+++ app/src/main/java/com/company/metrix/services/recomendations/FragmentRecomendations.kt
@@ -4,12 +4,22 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.company.metrix.BackButtonHandler
+import com.company.metrix.R
 import com.company.metrix.databinding.FragmentRecomendationsBinding
+import com.company.metrix.model.CharacteristicInfo
 import com.company.metrix.services.strenghts.CharacteristicFactoryImpl
 import com.company.metrix.services.strenghts.CharacteristicListAdapter
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.database.ktx.getValue
+import com.google.firebase.ktx.Firebase
 
 class FragmentRecomendations : Fragment() , BackButtonHandler{
 
@@ -36,14 +46,46 @@ class FragmentRecomendations : Fragment() , BackButtonHandler{
             adapter = weaknessAdapter
             layoutManager = LinearLayoutManager(activity)
         }
-        addDummyStrengths()
+        loadData()
     }
 
-    private fun addDummyStrengths(){
-        var i = 0
-        weaknessAdapter?.submitList(
-            CharacteristicFactoryImpl().produceAllStrengths().filter { i++ % 2 == 1 }
-        )
+    private fun loadData() {
+        val user = Firebase.auth.currentUser!!
+        Firebase.database.reference.child("users").orderByChild("id").equalTo(user.email)
+            .addListenerForSingleValueEvent(object :
+                ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    if (snapshot.children.iterator().hasNext()) {
+                        val comments: MutableList<String> =
+                            snapshot.children.iterator().next().child("comments")
+                                .getValue<MutableList<String>>() ?: mutableListOf()
+                        val list = comments.map {
+                            CharacteristicInfo(text = it, emoji = "\uD83D\uDCAC")
+                        }
+                        if (list.size > 0) {
+                            weaknessAdapter?.submitList(list)
+                        } else {
+                            binding?.emptyView?.visibility = View.VISIBLE
+                        }
+                        binding?.loadingBar?.visibility = View.INVISIBLE
+                        binding?.recommendationContent?.visibility = View.VISIBLE
+                    } else {
+                        showErrorToast()
+                    }
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    showErrorToast()
+                }
+            })
+    }
+
+    private fun showErrorToast() {
+        Toast.makeText(
+            requireContext(),
+            getString(R.string.database_error),
+            Toast.LENGTH_SHORT
+        ).show()
     }
 
     override fun setupOnBackButtonPressed() {
