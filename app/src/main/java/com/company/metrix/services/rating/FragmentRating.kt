180b988533954d372c73f523bbfd594de0a26aef
fedor-jedi <fedor.contact@yandex.ru>
2021-09-26 03:37:51 +0300

    Implemented strengths view

--- app/src/main/java/com/company/metrix/services/rating/FragmentRating.kt
+++ app/src/main/java/com/company/metrix/services/rating/FragmentRating.kt
@@ -75,7 +75,7 @@ class FragmentRating : Fragment(), BackButtonHandler {
     private fun updateRatingView(ratings: List<Double>) {
         val average = ratings.average()
         binding?.ratingBar?.rating = average.toFloat()
-        binding?.averageRating?.text = average.toString()
+        binding?.averageRating?.text = String.format("%.02f", average)
         binding?.ratingCount?.text = getString(R.string.estimates_count) + " " + ratings.size
         binding?.loadingBar?.visibility = View.INVISIBLE
         binding?.ratingContent?.visibility = View.VISIBLE
