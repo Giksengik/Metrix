d6e76fec07444d12223d33dd0c094785ac7cb528
Yana Glad <55493845+YanaGlad@users.noreply.github.com>
2021-10-03 23:16:06 +0300

    Проставила AndroidEntryPoints

--- app/src/main/java/com/company/metrix/MainActivity.kt
+++ app/src/main/java/com/company/metrix/MainActivity.kt
@@ -3,7 +3,9 @@ package com.company.metrix
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.company.metrix.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
 
