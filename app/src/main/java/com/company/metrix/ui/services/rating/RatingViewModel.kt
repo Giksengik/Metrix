bbee305bfaa929f7f934d32fe424ca8b136820fc
Yana Glad <55493845+YanaGlad@users.noreply.github.com>
2021-10-06 01:18:07 +0300

    Считается рейтинг

--- app/src/main/java/com/company/metrix/ui/services/rating/RatingViewModel.kt
+++ app/src/main/java/com/company/metrix/ui/services/rating/RatingViewModel.kt
@@ -1,9 +1,22 @@
 package com.company.metrix.ui.services.rating
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.company.metrix.data.repository.EstimationRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class RatingViewModel @Inject constructor() : ViewModel() {
+class RatingViewModel @Inject constructor(val repository: EstimationRepository) : ViewModel() {
+
+    val rating: MutableLiveData<List<Double>> = MutableLiveData<List<Double>>()
+
+    suspend fun calculateRating() {
+        val list = repository.getAllEstimations()
+        var rate: MutableList<Double> = arrayListOf()
+        for (est in list) {
+            rate.add(est.rate)
+        }
+        rating.value = rate
+    }
 }
