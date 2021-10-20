27bc6e06160c2fdf913431e201159ff0f6544dd9
Yana Glad <55493845+YanaGlad@users.noreply.github.com>
2021-10-20 17:38:36 +0300

    Swipe to action layout

--- app/src/main/java/com/company/metrix/ui/pulse/PulseQuestionsListAdapter.kt
+++ app/src/main/java/com/company/metrix/ui/pulse/PulseQuestionsListAdapter.kt
@@ -2,17 +2,23 @@ package com.company.metrix.ui.pulse
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.company.metrix.databinding.PulseItemBinding
 import com.company.metrix.data.model.PulseQuestion
+import com.company.metrix.data.model.User
 
 
-class PulseQuestionsListAdapter : ListAdapter<PulseQuestion, PulseQuestionsListAdapter.ViewHolder>
-    (PulseQuestionDiffUtil()) {
+class PulseQuestionsListAdapter(val onClick: OnPulseQuestionsListener) :
+    ListAdapter<PulseQuestion, PulseQuestionsListAdapter.ViewHolder>
+        (PulseQuestionDiffUtil()) {
 
+    interface OnPulseQuestionsListener {
+        fun onPulseClick(value: String, position: Int)
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder =
         ViewHolder(
@@ -20,14 +26,15 @@ class PulseQuestionsListAdapter : ListAdapter<PulseQuestion, PulseQuestionsListA
                 LayoutInflater.from(parent.context),
                 parent,
                 false
-            )
+            ),
+            onClick
         )
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.bind(getItem(position))
     }
 
-    class PulseQuestionDiffUtil : DiffUtil.ItemCallback<PulseQuestion>(){
+    class PulseQuestionDiffUtil : DiffUtil.ItemCallback<PulseQuestion>() {
         override fun areItemsTheSame(oldItem: PulseQuestion, newItem: PulseQuestion): Boolean =
             oldItem.question == newItem.question
 
@@ -36,10 +43,23 @@ class PulseQuestionsListAdapter : ListAdapter<PulseQuestion, PulseQuestionsListA
 
     }
 
-    class ViewHolder(val binding :PulseItemBinding) : RecyclerView.ViewHolder(binding.root){
-        fun bind(pulseQuestion: PulseQuestion){
+    class ViewHolder(
+        val binding: PulseItemBinding,
+        val onClickQuestion: OnPulseQuestionsListener
+    ) : RecyclerView.ViewHolder(binding.root) {
+
+
+        fun bind(pulseQuestion: PulseQuestion) {
             binding.questionValue.text = pulseQuestion.question
-            val answersListAdapter = PulseAnswersListAdapter()
+
+            val pos = position
+
+             val onClick = object : PulseAnswersListAdapter.OnPulseAnswersListener {
+                override fun onPulseClick(value: String, position: Int) {
+                    onClickQuestion.onPulseClick(value, pos)
+                }
+            }
+            val answersListAdapter = PulseAnswersListAdapter(onClick)
             binding.answersList.apply {
                 adapter = answersListAdapter
                 layoutManager = LinearLayoutManager(binding.root.context)
