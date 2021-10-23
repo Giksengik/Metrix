693015ea79dc975787461403880391eda2409760
Yana Glad <55493845+YanaGlad@users.noreply.github.com>
2021-10-23 19:36:17 +0300

    Выбор команд и адаптер для результатов (test)

---
+++ app/src/main/java/com/company/metrix/ui/servicesEmployer/diagnosticResult/ResultsAdapter.kt
@@ -0,0 +1,48 @@
+package com.company.metrix.ui.servicesEmployer.diagnosticResult
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.company.metrix.databinding.DiagnosticResultItemBinding
+
+class ResultsAdapter(
+) : ListAdapter<String, ResultsAdapter.ViewHolder>(
+    SkillDiffUtil()
+) {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder =
+        ViewHolder(
+            DiagnosticResultItemBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    class SkillDiffUtil : DiffUtil.ItemCallback<String>() {
+        override fun areItemsTheSame(oldItem: String, newItem: String): Boolean =
+            oldItem == newItem
+
+        override fun areContentsTheSame(oldItem: String, newItem: String): Boolean =
+            oldItem == newItem
+    }
+
+    class ViewHolder(
+        val binding: DiagnosticResultItemBinding,
+    ) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(value: String) {
+
+
+        }
+    }
+
+}
